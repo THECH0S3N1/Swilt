6e206dd96fc830ff11cf7b865f74ab1efd57b37e
Piotr Zaitev <piotrz@beast.fritz.box>
2018-05-29 01:29:05 +0200

    Initial Commit

---
+++ Swilt/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Swilt
+//
+//  Created by Piotr Zaitev on 29/05/2018.
+//  Copyright © 2018 NI Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
